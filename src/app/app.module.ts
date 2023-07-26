7617821cd11a9ae6eac5ecb33b5d23c0d2c644b2
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-07-26 17:56:48 +0300

    created the first dashboard page under modules folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent
+    DefaultComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
