409affd6580aacd542db406c6b1da54d543ffaab
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-07-26 22:00:16 +0300

    add header componentin the shared components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
 
   ],
   imports: [
