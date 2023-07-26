5995e75f6a2dc34f25908a4380054fe30619ccdc
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-07-26 18:01:08 +0300

    removed the declaration and imports of the created components in tha app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DefaultComponent } from './layouts/default/default.component';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent,
-    DashboardComponent
+
   ],
   imports: [
     BrowserModule,
