33f50d1214e313b82db1a42a130c8cd7621843fd
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-07-26 22:00:58 +0300

    add footer componentin the shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
 
   ],
   imports: [
